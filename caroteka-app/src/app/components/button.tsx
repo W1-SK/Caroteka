1a0c042a26d2c1d61c140f898a6b312bb51d0f1e
Zen <129403210+oneiros26@users.noreply.github.com>
2026-01-23 11:42:10 +0100

    Small fixes

--- caroteka-app/src/app/components/button.tsx
+++ caroteka-app/src/app/components/button.tsx
@@ -1,35 +1,33 @@
-import '../../index.css'
-import { Link } from 'react-router'
+import "../../index.css";
+import { Link } from "react-router";
 
 interface ButtonProps {
-    text: string
-    color: 'primary' | 'secondary' | 'danger' | 'success'
-    to?: string
+  text: string;
+  color: "primary" | "secondary" | "danger" | "success";
+  to?: string;
 }
 
 const colorClasses = {
-    primary: 'bg-violet-400',
-    secondary: 'bg-emerald-400',
-    danger: 'bg-rose-600',
-    success: 'bg-green-500',
-}
+  primary: "bg-violet-400",
+  secondary: "bg-emerald-400",
+  danger: "bg-rose-600",
+  success: "bg-green-500",
+};
 
-function Button(props: ButtonProps){
-    const className = `${colorClasses[props.color]} font-action text-button-m rounded-full py-1 px-3 inline-block`;
+function Button(props: ButtonProps) {
+  const className = `${
+    colorClasses[props.color]
+  } font-action text-button-m rounded-full py-1 px-3 inline-block no-underline`;
 
-    if (props.to) {
-        return (
-            <Link to={props.to} className={className}>
-                {props.text}
-            </Link>
-        )
-    }
-
+  if (props.to) {
     return (
-        <button className={className}>
-            {props.text}
-        </button>
-    )
+      <Link to={props.to} className={className}>
+        {props.text}
+      </Link>
+    );
+  }
+
+  return <button className={className}>{props.text}</button>;
 }
 
-export default Button
+export default Button;
--- caroteka-app/src/app/components/textBox.tsx
+++ caroteka-app/src/app/components/textBox.tsx
@@ -1,34 +1,36 @@
-import '../../index.css'
+import "../../index.css";
 
 interface TextBoxProps {
-    heading: string,
-    text: string,
-    color: 'primary' | 'secondary' | 'tertiary'
-    side: 'left' | 'right'
+  heading: string;
+  text: string;
+  color: "primary" | "secondary" | "tertiary";
+  side: "left" | "right";
 }
 
 const colorClasses = {
-    primary: 'bg-violet-300/40',
-    secondary: 'bg-emerald-500/20',
-    tertiary: 'bg-teal-400/20',
-}
+  primary: "bg-violet-300/40",
+  secondary: "bg-emerald-500/20",
+  tertiary: "bg-teal-400/20",
+};
 
 const leftOrRight = {
-    left: 'self-start',
-    right: 'self-end',
-}
+  left: "self-start",
+  right: "self-end",
+};
 
 function TextBox(props: TextBoxProps) {
-    const className = `${colorClasses[props.color]} w-120 p-4 rounded-2xl ${leftOrRight[props.side]}`;
+  const className = `${colorClasses[props.color]} w-120 p-4 rounded-2xl ${
+    leftOrRight[props.side]
+  }`;
 
-    return(
-        <section className="w-full flex flex-col">
-            <div className={className}>
-                <h2 className="text-h2 font-title">{props.heading}</h2>
-                <p>{props.text}</p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="w-full flex flex-col">
+      <div className={className}>
+        <h2 className="text-h2 font-title">{props.heading}</h2>
+        <p>{props.text}</p>
+      </div>
+    </section>
+  );
 }
 
 export default TextBox;
