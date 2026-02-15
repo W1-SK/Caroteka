7709144dd79657f9e4dd6389d04a39e0c2006dc3
Zen <129403210+oneiros26@users.noreply.github.com>
2026-02-15 06:29:47 +0100

    created change log
    thats it

---
+++ caroteka-app/src/app/components/logBox.tsx
@@ -0,0 +1,82 @@
+interface LogPageProps {
+  added: string[];
+  fixed: string[];
+  changed: string[];
+  removed: string[];
+  version: string;
+  date: string;
+  color: "primary" | "secondary" | "tertiary";
+}
+
+const colorClasses = {
+  primary: "bg-primary-100",
+  secondary: "bg-secondary-100",
+  tertiary: "bg-tertiary-100",
+};
+
+function LogBox({
+  added,
+  fixed,
+  changed,
+  removed,
+  version,
+  date,
+  color,
+}: LogPageProps) {
+  return (
+    <article
+      className={`flex flex-col gap-4 ${colorClasses[color]} w-2/5 p-8 rounded-xl`}
+    >
+      <section className="flex flex-col">
+        <h2 className="text-h2 font-title font-semibold">Čarotéka {version}</h2>
+        <p className="text-m">{date}</p>
+      </section>
+
+      {added.length > 0 && (
+        <section className="flex flex-col">
+          <h3 className="text-h5 font-title font-semibold">Přidáno</h3>
+          <ul className="list-disc pl-8">
+            {added.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </section>
+      )}
+
+      {fixed.length > 0 && (
+        <section className="flex flex-col">
+          <h3 className="text-h5 font-title font-semibold">Opraveno</h3>
+          <ul className="list-disc pl-8">
+            {fixed.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </section>
+      )}
+
+      {changed.length > 0 && (
+        <section className="flex flex-col">
+          <h3 className="text-h5 font-title font-semibold">Přetvořeno</h3>
+          <ul className="list-disc pl-8">
+            {changed.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </section>
+      )}
+
+      {removed.length > 0 && (
+        <section className="flex flex-col">
+          <h3 className="text-h5 font-title font-semibold">Vymazáno</h3>
+          <ul className="list-disc pl-8">
+            {removed.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </section>
+      )}
+    </article>
+  );
+}
+
+export default LogBox;
--- caroteka-app/src/app/pages/UpdateLogPage.tsx
+++ caroteka-app/src/app/pages/UpdateLogPage.tsx
@@ -1,14 +1,54 @@
-import '../../index.css'
 import Navbar from "../components/navbar.tsx";
+import Footer from "../components/footer.tsx";
+import LogBox from "../components/logBox.tsx";
 
 function UpdateLogPage() {
-    return (
-        <>
-            <Navbar pageTitle="Záznam změn" activePage=""/>
-            <p>coming soon, trust me bro</p>
-        </>
-    )
+  return (
+    <>
+      <Navbar pageTitle="Template" activePage="" />
+      <main className="w-full flex flex-col items-center gap-16 pt-72 pb-20">
+        <LogBox
+          version="0.0.0"
+          date="15. února 2026"
+          color="primary"
+          added={["vytvořena Čarotéka", "stránky pro registraci a přihlášení"]}
+          fixed={["žádné opravy, k tomu budou sloužit příští verze"]}
+          changed={["změny také žádné, kdo by to byl čekal u první verze"]}
+          removed={[
+            "doufám že v týhle kategorii toho do budoucna moc nebude, každé odstranění znamená ztracená práce na tom co se odstranilo..",
+          ]}
+        />
+        <LogBox
+          version="0.0.0"
+          date="15. února 2026"
+          color="secondary"
+          added={["neco"]}
+          fixed={[]}
+          changed={["neco3"]}
+          removed={[]}
+        />
+        <LogBox
+          version="0.0.0"
+          date="15. února 2026"
+          color="tertiary"
+          added={[]}
+          fixed={["neco2"]}
+          changed={[]}
+          removed={[]}
+        />
+        <LogBox
+          version="0.0.0"
+          date="15. února 2026"
+          color="primary"
+          added={["neco"]}
+          fixed={["neco2"]}
+          changed={[]}
+          removed={[]}
+        />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-
-export default UpdateLogPage
+export default UpdateLogPage;
