d5fac4d0cb7b118e0cb8e4eaf583c57109d88ab2
Zen <sobisekvit@gmail.com>
2026-04-23 11:09:58 +0200

    a tak

--- backend/src/validations/class.validation.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const createClassSchema = z.object({
-    body: z.object({
-        name: z.string().min(2),
-        source: z.string(),
-        data: z.any().optional(),
-    }),
-});
